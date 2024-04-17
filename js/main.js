2defe2a7748c6c7e67f14c445e29afa80314c07d
Akim Minko <akim.minko@yandex.ru>
2024-04-17 20:53:37 +0300

    style: white SVG

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// закрытие открытого collapse при открытии нового collapse
 document.addEventListener('DOMContentLoaded', () => {
               
   const collapseElements = document.querySelectorAll('#target-container .collapse');
@@ -13,4 +14,25 @@ document.addEventListener('DOMContentLoaded', () => {
     collapse.toggle();
   });
 
+});
+
+// окрашивание svg при наведении
+document.querySelectorAll("img.img-svg").forEach(function(img) {
+  let imgClass = img.getAttribute("class");
+  let imgURL = img.getAttribute("src");
+  fetch(imgURL)
+    .then(response => response.text())
+    .then(data => {
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(data, "application/xml");
+      let svg = doc.querySelector("svg");
+      if (typeof imgClass !== "undefined") {
+        svg.setAttribute("class", imgClass + " replaced-svg");
+      }
+      svg.removeAttribute("xmlns:a");
+      if (!svg.hasAttribute("viewBox") && svg.hasAttribute("height") && svg.hasAttribute("width")) {
+        svg.setAttribute("viewBox", "0 0 " + svg.getAttribute("height") + " " + svg.getAttribute("width"));
+      }
+      img.parentNode.replaceChild(svg, img);
+    });
 });
